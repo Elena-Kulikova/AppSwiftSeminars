6b7d8a5b5a96380fd2394e0996acd1810fafca8f
Elena-Kulikova <elena_komb@mail.ru>
2024-03-03 14:32:58 +0200

    Added solution to task from Seminar1

---
+++ Seminar1/HW1_Project2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HW1_Project2
+//
+//  Created by Elena on 29.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField1: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
